6a80bfc805fbe1bb0820c22ae6a2fbf9c3c91dc9
tweed1e <jesse.tweedle@gmail.com>
2022-02-02 13:36:36 -0500

    p good tags doc

--- src/pyncparser/parse.py
+++ src/pyncparser/parse.py
@@ -1,67 +1,29 @@
-# function that parses a thing
-# not anywhere near a real package yet
-# still work just on a script
-
-# open tags v. start-end tags
-# start-end tags, easy
-# open tags, easy as long as you know what they are.
-# print the manual and check. cross-check with rust parser
-
-# manual: lists all tags, differentiates between tags and end-tags
-# submission header tag elements, document header tag elements, 
-# document header and text tags
-# lists information and format for each tag
-# in that information, has "END TAG", either the end tag or "NA"
-# so that's something, just need machine readable format for this thing
-
-# none of these worked. either patch up the sgml=>xml on the fly, or write my
-# own parser? which one's easier? which one's faster?
-
-# from lxml import etree
-import os
 import pandas as pd
-from pyparsing import col
-# from io import StringIO, BytesIO
-# ugh idk what happening
 
+def format_tag_csv(fname):
+    dtypes = {
+        'data_element': str,
+        'tag': str,
+        'description': str,
+        'length': str,
+        'end_tag': str,
+        'characteristic': str,
+        'limits': str,
+        'format': str
+    }
+    tags = pd.read_csv(fname, dtype = dtypes)
+    return tags[["tag", "end_tag"]]
+
+    
 def main():
 
-    # parser = etree.XMLParser(load_dtd = True)
-    # parser.resolvers.add(DTDResolver())
-
     with open("src/pyncparser/test/data/9999999997-22-000253.nc", mode = "rt") as f:
-    # with open("test/data/9999999997-22-000253.nc", mode = "rt") as f:
         text = f.read()
     
-    cc = pd.read_csv("pds-tags.csv")
-    print(cc.head())
-    print(cc.loc[[2]].transpose())
-    # alright, in theory I have the tags and end tags, and that's all fi need
-    print(cc[["TAG", "END TAG"]])
-    # print(cc.columns)
-    # now I have every tag and whether they have an end tag or not
-    # except footnotes F1-F99
-    # also what they mean; e.g., page means nothing
-    
-    
-    # strip, standardize, etc.
-    # with open("copy-edgar-pds.dtd", mode = "rt") as g:
-    # # with open("test/data/9999999997-22-000253.nc", mode = "rt") as f:
-    #     dtd = g.read()
+    tags = format_tag_csv("pds-tags-pd.csv")
 
-    # text = '<!DOCTYPE note SYSTEM "edgar-pds.dtd">\n' + text
-    # text = dtd + text
+    # now go through that thing and check them all
 
-    # print(text[:100])
-    # tree = etree.parse("src/pyncparser/pds.txt", parser)
-    # tree = etree.parse(StringIO(text), parser)
-    # tree = etree.parse(StringIO(text), parser)
-    # print(tree)
-
-    # j = parse(text)
-    # print(j)
-
-    # strategies: try to read it myself, or just try to smash the DTD into lxml?
 
 if __name__ == "__main__":
     main()
